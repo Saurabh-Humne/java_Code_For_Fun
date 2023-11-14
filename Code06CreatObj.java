464e8b29d1d4fc89526d233d58f9cbd6e4a53275
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2023-11-14 15:58:39 +0530

    Add files via upload

---
+++ Code06CreatObj.java
@@ -0,0 +1,18 @@
+// this is to show that how to creat an object
+
+class Code06CreatObj {
+ void show() {
+  System.out.println("Lets begin OOP");
+ }
+
+ public static void main(String[] args){
+  Code06CreatObj obj = new Code06CreatObj();
+  obj.show();
+ }
+}
+
+/* Output
+
+Lets begin OOP
+
+*/

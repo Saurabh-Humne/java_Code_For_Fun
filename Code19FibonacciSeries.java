21d79858c04770051af2f080e4fa5a2bd05f48b5
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2023-12-13 21:09:10 +0530

    Create Code19FibonacciSeries.java

---
+++ Code19FibonacciSeries.java
@@ -0,0 +1,30 @@
+// print Fibonacci Series using recursion
+
+class Code19FibonacciSeries {
+ static int n1=0,n2=1,n3=0;
+
+ static void printFibonacci(int count){
+  if(count>0){
+   n3 = n1+n2;
+   n1 = n2;
+   n2 = n3;
+
+   System.out.print(" "+ n3);
+   printFibonacci(count-1);
+  }
+ }
+
+ public static void main(String[] args){
+  int count = 10;
+
+  System.out.print(n1+" "+n2);
+
+  printFibonacci(count-2);
+ }
+}
+
+/* Output
+
+0 1 1 2 3 5 8 13 21 34
+
+*/

8f4aaa3e0754b5219cbbfdf7b81eb8bf1cb1b3b9
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2024-01-14 22:55:04 +0530

    Create Code35Pattrn.java

---
+++ Code35Pattrn.java
@@ -0,0 +1,45 @@
+//Create a java star triangle in between numbers
+public class Code35Pattrn {
+	public static void main(String[] args) {
+		int lines = 4;
+		int i,j;
+		int space = 0;
+		for(i=0;i<lines;i++) {
+			for(j=1;j<=space;j++) {
+				System.out.print(" ");
+			}
+			for(j=1;j<=lines;j++) {
+				if(j<=(lines-i)) 
+					System.out.print(j);
+				 else 
+					System.out.print("*");
+				
+			}
+		
+				j--;
+		
+				while(j>0) {
+					if(j>lines-i) 
+						System.out.print("*");
+					 else 
+						System.out.print(j);
+					
+					j--;
+				}
+				
+				if((lines-i)>9) 
+					space=space+1;
+					System.out.println("");
+				
+			}
+	}
+	
+}
+
+
+/* Output
+12344321
+123**321
+12****21
+1******1
+*/
